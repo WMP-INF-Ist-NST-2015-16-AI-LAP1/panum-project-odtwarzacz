bd7abd6888f8dd357769965fd7bd56ad0ae87045
unknown <adrian95socha@o2.pl>
2018-12-31 18:18:00 +0100

    1

---
+++ Odtwarzacz/app/src/main/java/com/example/adria/odtwarzacz/MusicController.java
@@ -0,0 +1,71 @@
+package com.example.adria.odtwarzacz;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController implements MediaController.MediaPlayerControl {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+
+
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+}
